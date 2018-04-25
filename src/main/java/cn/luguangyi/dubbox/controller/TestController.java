00faadc46b2c8616ca896bd1005f2d0ba8202830
muqing <muqing@gmail.com>
2018-04-25 11:54:39 +0800

    Change domin to domain and test.

--- src/main/java/cn/luguangyi/dubbox/controller/TestController.java
+++ src/main/java/cn/luguangyi/dubbox/controller/TestController.java
@@ -1,18 +1,15 @@
 package cn.luguangyi.dubbox.controller;
 
 
-import cn.luguangyi.dubbox.domin.Person;
+import cn.luguangyi.dubbox.domain.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@EnableAutoConfiguration
 public class TestController {
 
     @Value("${spring.application.name}")
--- src/main/java/cn/luguangyi/dubbox/domain/Person.java
+++ src/main/java/cn/luguangyi/dubbox/domain/Person.java
@@ -1,8 +1,7 @@
-package cn.luguangyi.dubbox.domin;
+package cn.luguangyi.dubbox.domain;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Data
